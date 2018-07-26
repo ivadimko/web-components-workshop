01c3de7467ef4f4db046a44667b3731d1e73e549
Aleksey Shvayka <shvaikalesh@gmail.com>
2018-07-26 20:18:53 +0300

    initial

---
+++ relative-time/relative-time.js
@@ -0,0 +1,12 @@
+// API:
+// <relative-time dateTime="..." lang="..." />
+
+import moment from "moment"
+import "moment/locale/pt-br"
+import "moment/locale/ru"
+import "moment/locale/de"
+import "moment/locale/fr"
+import "moment/locale/uk"
+import "moment/locale/se"
+
+console.log(moment.locales())
